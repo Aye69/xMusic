9eeaa74a8c92c9ec3debe8eac123e790f9d5c0fc
Steven Apple <75445904+xStevenZero@users.noreply.github.com>
2021-03-21 18:11:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const MusicBot = require('discord-music-system');
 const fs = require('fs')
 const { config } = require("dotenv");
 const { Client, Collection } = require("discord.js");
-const { prefix } = require(process.env.PREFIX);
+const prefix = process.env.PREFIX
 
 const client = new Discord.Client();
 
