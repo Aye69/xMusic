6a816a83720ac9ebd6f8047b826acf140d15ed4d
Steven Apple <75445904+xStevenZero@users.noreply.github.com>
2021-03-21 18:24:26 +0100

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,1 +1,15 @@
-
+const Discord = require('discord.js')
+module.exports = {
+    name: 'help',
+    category: 'Help Commands',
+     run: async(client, message, args) => {
+         let help = new Discord.MessageEmbed()
+         .setTitle("Commands List")
+         .setDescription("**Prefix**: `$`")
+         .setTimestamp()
+         .addFields(
+         { name: 'Music' , value: '`play` `add` `join` `stop` `leave` `current` `queue` `skip` `volume` `pause` `resume` `lyrics`' },
+         )
+         message.channel.send(help)
+     } 
+}
